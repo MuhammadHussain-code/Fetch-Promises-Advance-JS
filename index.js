f44a5518b9a1b82d41be35f79954758a2cb17156
muhammadhussain-code <muhammadhussain99100@gmail.com>
2019-12-02 14:56:53 +0500

    class_2_dec_19

--- index.js
+++ index.js
@@ -43,3 +43,51 @@
     //     console.log(error);
     // })
 // }
+
+
+// let prom = _=>{
+//     fetch('https://jsonplaceholder.typicode.com/todos/')
+//     .then(result => result.json())
+//     .then(result => {
+//         console.log(document.body.childNodes.values)
+//         console.log(result[0])})
+//     .catch(error => {
+//       console.log(error)
+//     }) 
+//   }
+//   prom()
+//   console.log("hello")
+
+
+// Async Await
+
+// async function asd() {}
+
+// let prom = async _ =>{
+// try {
+//   let responsejson = await fetch('https://jsonplaceholder.typicode.com/todos/');
+//   let result = await responsejson.json(); 
+//   console.log(result)
+// }catch(err) {
+//   console.log(err)
+// }
+  
+// }
+
+// (async _=> {
+//   try {
+//     let responsejson = await fetch('https://jsonplaceholder.typicode.com/todos/');
+//     let result = await responsejson.json(); 
+//     console.log(result)
+//   }catch(err) {
+//     console.log(err)
+//   } 
+// })()
+
+
+let asd = new Promise((resolve, reject)=> {
+  resolve(prom())
+}); 
+asd.then(_ => {
+  console.log("hello")
+})
